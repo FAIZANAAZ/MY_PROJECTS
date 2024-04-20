2497945dddd05467f2618cc4338ca63f8324dd41
FAIZANAAZ <github.com>
2024-04-20 05:15:41 -0700

    first commit

--- SIMPLE_CALCULATOR/index.ts
+++ SIMPLE_CALCULATOR/index.ts
@@ -1,29 +1,39 @@
 #!/usr/bin/env node
 
 import inquirer from "inquirer";
+import chalk from "chalk";
 
 const anwser = await inquirer.prompt([
   { message: "Enter first number ", type: "number", name: "firstnumber" },
+
   { message: "Enter second number ", type: "number", name: "secondtnumber" },
+
   { message: "Select one of the operators to perform operation ", type: "list", name : 'operators',
-choices:['Addition','Subtraction','Multiplication','Division']
+choices:['Addition','Subtraction','Multiplication','Division','Exponant','Module']
 },
 ]);
 
 
 if(anwser.operators === 'Addition'){
-    console.log( anwser.firstnumber + anwser.secondtnumber );
+    console.log( chalk.bgGreenBright.bold('\n\t ', anwser.firstnumber + anwser.secondtnumber ));
     
 }else if (anwser.operators === 'Subtraction'){
-    console.log( anwser.firstnumber - anwser.secondtnumber );
+    console.log( '\n\t ',anwser.firstnumber - anwser.secondtnumber );
 }
     else if (anwser.operators === 'Multiplication'){
-        console.log( anwser.firstnumber * anwser.secondtnumber );
+        console.log(chalk.bgGreenBright.bold('\n\t ', anwser.firstnumber * anwser.secondtnumber ));
     }
     else if (anwser.operators === 'Division'){
-        console.log( anwser.firstnumber / anwser.secondtnumber );
-    } else{
-        console.log( 'Please select a valid operator');
+        console.log(chalk.bgGreenBright.bold('\n\t ',anwser.firstnumber / anwser.secondtnumber ));
+    } 
+    else if (anwser.operators === 'Exponant'){
+        console.log(chalk.bgGreenBright.bold('\n\t ', anwser.firstnumber ** anwser.secondtnumber ));
+    }
+    else if (anwser.operators === 'Module'){
+        console.log(chalk.bgGreenBright.bold( '\n\t ',anwser.firstnumber % anwser.secondtnumber ));
+    }
+    else{
+        console.log( chalk.bgRedBright.bold('\n\t ','Please select a valid operator'));
     }
         
     
---
+++ WORDS_COUNTER/word_counter.js
@@ -0,0 +1,28 @@
+import chalk from "chalk";
+import inquirer from "inquirer";
+console.log(chalk.bgYellow.bold.italic.underline("\n\t Hello ! (WELCOME IN FAIZA_NAAZ(WORDS COUNTER))\n\t"));
+let quation = await inquirer.prompt([{
+        type: "confirm",
+        name: "yes$no",
+        message: "You want to count your sentence length ",
+        default: true
+    }]);
+if (quation.yes$no == true) {
+    console.log(chalk.bgBlue.bold("\n\t ok sure \n\t"));
+    let user_ans = await inquirer.prompt([
+        {
+            type: "input",
+            name: "words",
+            message: "please enter you sentences"
+        }
+    ]);
+    console.log("\n\t", chalk.bgMagentaBright.bold(user_ans.words) + "\n\t");
+    if (user_ans.words !== " ") {
+        let answer = user_ans.words.trim().split(" ");
+        console.log(answer);
+        console.log(chalk.bgGray.bold(`\n\t Your sentence words count is ${answer.length} words`));
+    }
+}
+else {
+    console.log(chalk.bgCyanBright.bold("\n\t OK BY"));
+}
